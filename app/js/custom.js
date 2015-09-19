e263656b3454766c48de550097a64595d96820ab
Aaron Biser <mail@aaronbiser.com>
2015-09-19 14:29:30 -0500

    moving carousel files

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() { 
-	
+			
 	//Owl Carousel Settings
-	/*$(".owl-carousel.homeboxes").owlCarousel({
+	$(".owl-carousel.homeboxes").owlCarousel({
 		navigation: true, // Show next and prev buttons
         slideSpeed: 300,
         paginationSpeed: 400,
@@ -10,7 +10,7 @@ $(document).ready(function() {
         autoPlay: 5000,
         stopOnHover: true,
         rewindNav: true
-	});*/
+	});
 	
 	
 	$(".dropdown-menu a").click(function() {
